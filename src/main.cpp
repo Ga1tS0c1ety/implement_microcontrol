5e6fe53d006f34e69551e54c1001af1292dc2a5c
Gait <ithielgabrieldjifadenyigba@gmail.com>
2025-10-06 18:19:58 +0200

    test code du robot

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,80 @@
-#include <Arduino.h>
+#include "Arduino.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 13
+#endif
+
+// --- Définition des broches pour les moteurs ---
+const int ENA = 9;   // Enable moteur gauche (PWM)
+const int IN1 = 8;   // Direction moteur gauche
+const int IN2 = 7;   // Direction moteur gauche
+
+const int ENB = 10;  // Enable moteur droit (PWM)
+const int IN3 = 12;  // Direction moteur droit
+const int IN4 = 11;  // Direction moteur droit
+
+// --- Prototypes des fonctions ---
+void avancer(int vitesse);
+void reculer(int vitesse);
+void stopRobot();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Initialisation des broches
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  // Démarre avec les moteurs arrêtés
+  stopRobot();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Fait avancer le robot pendant 3 secondes
+  avancer(200);
+  delay(3000);
+
+  // Stoppe le robot 1 seconde
+  stopRobot();
+  delay(1000);
+
+  // Fait reculer le robot pendant 3 secondes
+  reculer(200);
+  delay(3000);
+
+  // Stoppe de nouveau 2 secondes
+  stopRobot();
+  delay(2000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// --- Fonctions de mouvement ---
+
+void avancer(int vitesse) {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, vitesse);
+  analogWrite(ENB, vitesse);
 }
+
+void reculer(int vitesse) {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENA, vitesse);
+  analogWrite(ENB, vitesse);
+}
+
+void stopRobot() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+}
+
